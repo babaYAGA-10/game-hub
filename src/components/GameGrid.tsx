fb40066e8e4f13577bfe5d22050c89865ddccb81
babaYAGA-10 <sonivarun2001@gmail.com>
2023-08-29 13:01:08 +0530

    Display genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,14 +14,14 @@ const GameGrid = () => {
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
-        spacing={10}
+        spacing={3}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer>
               <GameCardSkeleton key={skeleton} />
             </GameCardContainer>
-          ))}
+         ))}
         {data.map((game) => (
             <GameCardContainer>
                 <GameCard key={game.id} game={game} />
