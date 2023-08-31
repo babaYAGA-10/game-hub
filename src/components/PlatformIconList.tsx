7a6a8b9f433691f08a9ef12692d05774df3e21fe
babaYAGA-10 <sonivarun2001@gmail.com>
2023-08-31 13:10:11 +0530

    Filter games by genre

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -38,7 +38,7 @@ const PlatformIconList = ({ platforms }: Props) => {
     // {1} == '4px' in chakra
     <HStack marginY={1}>
       {platforms.map((platform) => (
-        <Icon as={iconMap[platform.slug]} color='gray.500' />
+        <Icon key={platform.id} as={iconMap[platform.slug]} color='gray.500' />
       ))}
     </HStack>
   );
